c0ca759f16a7cd6c73e6672271ee9d9b3ce582aa
Andrii Yaremenko <yarrem@gmail.com>
2022-07-28 23:43:31 +0300

    adds initial README

---
+++ README.go
@@ -0,0 +1,12 @@
+# controller
+
+This package is aimed to reduce amount of boilerplate code when writing regular http.Handle.
+It provides simple and explicit API to define HTTP endpoints.
+
+### To install controller:
+`go get -u github.com/andriiyaremenko/controller`
+
+### How to use:
+```go
+// TODO: write how to use
+```
